c9bd58735ffc80238103331398aecd8d62e136a5
Inioluwa Olaleye <olaleyeini7@gmail.com>
2023-01-29 17:33:35 -0600

    place signin and header component in welcomePage.tsx

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,20 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/header";
-import SignIn from "./components/signin";
-import Navbar from "./components/navbar";
+
+import WelcomePage from "./pages/welcomePage"
 import Register from "./AppRegisterPage"
-// import { Link, Routes, Route } from 'react-router-dom';
-import { Sign } from 'crypto';
+
 
 function App() {
   return (
 
   <Router>
     <Routes>
-    {/* <Route path="/" element={<Header/>}></Route> */}
-    <Route path="/" element={<SignIn />}/>
+    
+    <Route path="/" element={<WelcomePage/>}/>
     <Route path="/Register" element={<Register/>}/>
 
     </Routes>
