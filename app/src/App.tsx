0ec825ea2f4e52c8562d70446e6cbcfb996e1f4b
Eroscupid <vutuanduong2003@yahoo.com>
2023-01-29 17:45:46 -0600

    fix app.tsx

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // import Login from './components/pages/login/login';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import WelcomePage from "./pages/welcomePage"
 import Register from "./AppRegisterPage"
 
