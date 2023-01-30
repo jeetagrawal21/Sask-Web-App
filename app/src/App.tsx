89842cc32d0a6cd6046a3d37942ba1face568d93
Eroscupid <vutuanduong2003@yahoo.com>
2023-01-29 19:08:15 -0600

    push some template into dashboard

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // import Login from './components/pages/login/login';
 import './App.css';
 import WelcomePage from "./pages/welcomePage"
 import Register from "./AppRegisterPage"
+import Dashboard from './components/layout/defaultLayout';
 
 
 
@@ -14,6 +15,7 @@ function App() {
     <Routes>
     
     <Route path="/" element={<WelcomePage/>}/>
+    <Route path="/dashboard" element={<Dashboard/>}/>
     <Route path="/Register" element={<Register/>}/>
 
     </Routes>
