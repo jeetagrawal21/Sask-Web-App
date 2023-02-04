da43c495a75b7d5c8dbad7414e71c3484256e213
Eroscupid <vutuanduong2003@yahoo.com>
2023-02-03 19:24:07 -0600

    change

---
+++ api/src/data-source.ts
@@ -0,0 +1,18 @@
+import { DataSource } from "typeorm";
+import "reflect-metadata";
+import { User } from "./entity/User";
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "postgres",
+  password: "ThisIsASuperLongAndCoolPassword4DevelopmentToKeepOutHackersSoIfYoureAHackerPleaseLeave>:3",
+  database: "SaskLongCovidData",
+  synchronize: true,
+  logging: true,
+  entities: [User],
+  subscribers: [],
+  migrations: [],
+});
+
