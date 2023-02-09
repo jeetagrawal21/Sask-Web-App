9702e1d8fdc8d478c5214f7cb0f31524773915af
harichristensen <harilfc@gmail.com>
2023-02-09 14:13:59 -0600

    Added security q fields, need to work on alignment

--- app/src/auth/pages/Register.tsx
+++ app/src/auth/pages/Register.tsx
@@ -1,10 +1,5 @@
 import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
-import FormControl from "@material-ui/core/FormControl";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import FormLabel from "@material-ui/core/FormLabel";
-import Radio from "@material-ui/core/Radio";
-import RadioGroup from "@material-ui/core/RadioGroup";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
 import LoadingButton from "@material-ui/lab/LoadingButton";
@@ -12,16 +7,12 @@ import { useFormik } from "formik";
 import { useTranslation } from "react-i18next";
 import { Link, useNavigate } from "react-router-dom";
 import * as Yup from "yup";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 import BoxedLayout from "../../core/components/BoxedLayout";
 import { useSnackbar } from "../../core/contexts/SnackbarProvider";
 import { useRegister } from "../hooks/useRegister";
 import { UserInfo } from "../types/userInfo";
-
-const genders = [
-  { label: "auth.register.form.gender.options.f", value: "F" },
-  { label: "auth.register.form.gender.options.m", value: "M" },
-  { label: "auth.register.form.gender.options.n", value: "NC" },
-];
 
 const Register = () => {
   const navigate = useNavigate();
@@ -30,22 +21,50 @@ const Register = () => {
 
   const { isRegistering, register } = useRegister();
 
+  
+
   const formik = useFormik({
     initialValues: {
       email: "",
-      firstName: "",
-      gender: "F",
-      lastName: "",
+      givenName1: "",
+      givenName2: "",
+      surname: "",
+      question1: "",
+      question2: "",
+      question3: "",
+      answer1: "",
+      answer2: "",
+      answer3: "",
     },
     validationSchema: Yup.object({
       email: Yup.string()
         .email("Invalid email address")
         .required(t("common.validations.required")),
-      firstName: Yup.string()
+      givenName1: Yup.string()
         .max(20, t("common.validations.max", { size: 20 }))
         .required(t("common.validations.required")),
-      lastName: Yup.string()
+      givenName2: Yup.string()
+        .max(20, t("common.validations.max", { size: 20 })),
+        surname: Yup.string()
         .max(30, t("common.validations.max", { size: 30 }))
+        .required(t("common.validations.required")),
+      question1: Yup.string()
+        .max(20, t("common.validations.max", { size: 100 }))
+        .required(t("common.validations.required")),
+      question2: Yup.string()
+        .max(20, t("common.validations.max", { size: 100 }))
+        .required(t("common.validations.required")),
+      question3: Yup.string()
+        .max(20, t("common.validations.max", { size: 100 }))
+        .required(t("common.validations.required")),
+      answer1: Yup.string()
+        .max(20, t("common.validations.max", { size: 40 }))
+        .required(t("common.validations.required")),
+      answer2: Yup.string()
+        .max(20, t("common.validations.max", { size: 40 }))
+        .required(t("common.validations.required")),
+      answer3: Yup.string()
+        .max(20, t("common.validations.max", { size: 40 }))
         .required(t("common.validations.required")),
     }),
     onSubmit: (values) => handleRegister(values),
@@ -54,6 +73,7 @@ const Register = () => {
   const handleRegister = async (values: Partial<UserInfo>) => {
     register(values as UserInfo)
       .then(() => {
+        console.log(values)
         snackbar.success(t("auth.register.notifications.success"));
         navigate(`/${process.env.PUBLIC_URL}/login`);
       })
@@ -62,8 +82,10 @@ const Register = () => {
       });
   };
 
+
+
   return (
-    <BoxedLayout>
+    <Grid maxWidth={"100%"} component={Paper} square>
       <Typography component="h1" variant="h5">
         {t("auth.register.title")}
       </Typography>
@@ -72,58 +94,53 @@ const Register = () => {
         marginTop={3}
         noValidate
         onSubmit={formik.handleSubmit}
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100%',
+        }}
       >
         <TextField
           margin="normal"
           required
           fullWidth
-          id="lastName"
-          label={t("auth.register.form.lastName.label")}
-          name="lastName"
+          id="surname"
+          label="Surname"
+          name="surname"
           autoComplete="family-name"
           autoFocus
           disabled={isRegistering}
-          value={formik.values.lastName}
+          value={formik.values.surname}
           onChange={formik.handleChange}
-          error={formik.touched.lastName && Boolean(formik.errors.lastName)}
-          helperText={formik.touched.lastName && formik.errors.lastName}
+          error={formik.touched.surname && Boolean(formik.errors.surname)}
+          helperText={formik.touched.surname && formik.errors.surname}
         />
         <TextField
           margin="normal"
           required
-          fullWidth
-          id="firstName"
-          label={t("auth.register.form.firstName.label")}
+          label="Given Name 1"
           name="firstName"
           autoComplete="given-name"
           disabled={isRegistering}
-          value={formik.values.firstName}
+          value={formik.values.givenName1}
           onChange={formik.handleChange}
-          error={formik.touched.firstName && Boolean(formik.errors.firstName)}
-          helperText={formik.touched.firstName && formik.errors.firstName}
+          error={formik.touched.givenName1 && Boolean(formik.errors.givenName1)}
+          helperText={formik.touched.givenName1 && formik.errors.givenName1}
         />
-        <FormControl component="fieldset" margin="normal">
-          <FormLabel component="legend">
-            {t("auth.register.form.gender.label")}
-          </FormLabel>
-          <RadioGroup
-            row
-            aria-label="gender"
-            name="gender"
-            value={formik.values.gender}
-            onChange={formik.handleChange}
-          >
-            {genders.map((gender) => (
-              <FormControlLabel
-                control={<Radio />}
-                key={gender.value}
-                disabled={isRegistering}
-                label={t(gender.label)}
-                value={gender.value}
-              />
-            ))}
-          </RadioGroup>
-        </FormControl>
+        <TextField
+          margin="normal"
+          id="givenName2"
+          label="Given Name 2"
+          name="givenName2"
+          autoComplete="given-name-2"
+          disabled={isRegistering}
+          value={formik.values.givenName2}
+          onChange={formik.handleChange}
+          error={formik.touched.givenName2 && Boolean(formik.errors.givenName2)}
+          helperText={formik.touched.givenName2 && formik.errors.givenName2}
+        />
         <TextField
           margin="normal"
           required
@@ -138,6 +155,35 @@ const Register = () => {
           error={formik.touched.email && Boolean(formik.errors.email)}
           helperText={formik.touched.email && formik.errors.email}
         />
+
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="question1"
+          label="Security Question 1"
+          name="question1"
+          disabled={isRegistering}
+          value={formik.values.question1}
+          onChange={formik.handleChange}
+          error={formik.touched.question1 && Boolean(formik.errors.question1)}
+          helperText={formik.touched.question1 && formik.errors.question1}
+        />
+
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="answer1"
+          label="Security Answer 1"
+          name="answer1"
+          disabled={isRegistering}
+          value={formik.values.answer1}
+          onChange={formik.handleChange}
+          error={formik.touched.answer1 && Boolean(formik.errors.answer1)}
+          helperText={formik.touched.answer1 && formik.errors.answer1}
+        />
+        
         <LoadingButton
           type="submit"
           fullWidth
@@ -159,7 +205,7 @@ const Register = () => {
           {t("auth.register.back")}
         </Button>
       </Box>
-    </BoxedLayout>
+      </Grid>
   );
 };
 
