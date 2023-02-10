ed05e4b3e16148a4b3daf3d00f5418c68f891d40
harichristensen <harilfc@gmail.com>
2023-02-09 18:19:21 -0600

    Finished register page for now, review in future

--- app/src/auth/pages/Register.tsx
+++ app/src/auth/pages/Register.tsx
@@ -9,7 +9,6 @@ import { Link, useNavigate } from "react-router-dom";
 import * as Yup from "yup";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
-import BoxedLayout from "../../core/components/BoxedLayout";
 import { useSnackbar } from "../../core/contexts/SnackbarProvider";
 import { useRegister } from "../hooks/useRegister";
 import { UserInfo } from "../types/userInfo";
@@ -85,8 +84,16 @@ const Register = () => {
 
 
   return (
-    <Grid maxWidth={"100%"} component={Paper} square>
-      <Typography component="h1" variant="h5">
+    <Grid maxWidth={"100%"} 
+      direction="row"
+      display="grid"
+      component={Paper} 
+      justifyItems="center"
+      alignContent="center"
+      square
+      sx={{ height: "100vh" }}
+      >
+      <Typography component="h1" variant="h3">
         {t("auth.register.title")}
       </Typography>
       <Box
@@ -100,91 +107,210 @@ const Register = () => {
           justifyContent: 'center',
           alignItems: 'center',
           height: '100%',
-          width: "50%"
+          width: "50%",
+          margin: "0"
         }}
       >
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          id="surname"
-          label="Surname"
-          name="surname"
-          autoComplete="family-name"
-          autoFocus
-          disabled={isRegistering}
-          value={formik.values.surname}
-          onChange={formik.handleChange}
-          error={formik.touched.surname && Boolean(formik.errors.surname)}
-          helperText={formik.touched.surname && formik.errors.surname}
-        />
-        <TextField
-          margin="normal"
-          required
-          label="Given Name 1"
-          name="firstName"
-          autoComplete="given-name"
-          disabled={isRegistering}
-          value={formik.values.givenName1}
-          onChange={formik.handleChange}
-          error={formik.touched.givenName1 && Boolean(formik.errors.givenName1)}
-          helperText={formik.touched.givenName1 && formik.errors.givenName1}
-        />
-        <TextField
-          margin="normal"
-          id="givenName2"
-          label="Given Name 2"
-          name="givenName2"
-          autoComplete="given-name-2"
-          disabled={isRegistering}
-          value={formik.values.givenName2}
-          onChange={formik.handleChange}
-          error={formik.touched.givenName2 && Boolean(formik.errors.givenName2)}
-          helperText={formik.touched.givenName2 && formik.errors.givenName2}
-        />
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          id="email"
-          label={t("auth.register.form.email.label")}
-          name="email"
-          autoComplete="email"
-          disabled={isRegistering}
-          value={formik.values.email}
-          onChange={formik.handleChange}
-          error={formik.touched.email && Boolean(formik.errors.email)}
-          helperText={formik.touched.email && formik.errors.email}
-        />
+        <div 
+          style={{
+            display: "flex",
+            width: "100%"
+          }}>
+          <TextField
+            margin="normal"
+            required
+            label="Given Name 1"
+            fullWidth
+            name="firstName"
+            autoComplete="given-name"
+            disabled={isRegistering}
+            value={formik.values.givenName1}
+            onChange={formik.handleChange}
+            error={formik.touched.givenName1 && Boolean(formik.errors.givenName1)}
+            helperText={formik.touched.givenName1 && formik.errors.givenName1}
+            style={{
+              marginRight:"10px"
+            }}
+          />
+          <TextField
+            margin="normal"
+            id="givenName2"
+            fullWidth
+            label="Given Name 2"
+            name="givenName2"
+            autoComplete="given-name-2"
+            disabled={isRegistering}
+            value={formik.values.givenName2}
+            onChange={formik.handleChange}
+            error={formik.touched.givenName2 && Boolean(formik.errors.givenName2)}
+            helperText={formik.touched.givenName2 && formik.errors.givenName2}
+            style={{
+              marginLeft:"10px"
+            }}
+          />
+        </div>
+        <div 
+          style={{
+            display: "flex",
+            width: "100%"
+          }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="surname"
+            label="Surname"
+            name="surname"
+            autoComplete="family-name"
+            autoFocus
+            disabled={isRegistering}
+            value={formik.values.surname}
+            onChange={formik.handleChange}
+            error={formik.touched.surname && Boolean(formik.errors.surname)}
+            helperText={formik.touched.surname && formik.errors.surname}
+            style={{
+              marginRight:"10px"
+            }}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label={t("auth.register.form.email.label")}
+            name="email"
+            autoComplete="email"
+            disabled={isRegistering}
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            error={formik.touched.email && Boolean(formik.errors.email)}
+            helperText={formik.touched.email && formik.errors.email}
+            style={{
+              marginLeft:"10px"
+            }}
+          />
+        </div>
+        <Typography component="h1" variant="h5" style={{marginTop:"20px"}}>
+          Security Questions
+        </Typography>
+        <div 
+          style={{
+            display: "flex",
+            width: "100%"
+          }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="question1"
+            label="Security Question 1"
+            name="question1"
+            disabled={isRegistering}
+            value={formik.values.question1}
+            onChange={formik.handleChange}
+            error={formik.touched.question1 && Boolean(formik.errors.question1)}
+            helperText={formik.touched.question1 && formik.errors.question1}
+            style={{
+              marginRight:"10px"
+            }}
+            
+          />
 
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          id="question1"
-          label="Security Question 1"
-          name="question1"
-          disabled={isRegistering}
-          value={formik.values.question1}
-          onChange={formik.handleChange}
-          error={formik.touched.question1 && Boolean(formik.errors.question1)}
-          helperText={formik.touched.question1 && formik.errors.question1}
-        />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="answer1"
+            label="Security Answer 1"
+            name="answer1"
+            disabled={isRegistering}
+            value={formik.values.answer1}
+            onChange={formik.handleChange}
+            error={formik.touched.answer1 && Boolean(formik.errors.answer1)}
+            helperText={formik.touched.answer1 && formik.errors.answer1}
+            style={{
+              marginLeft:"10px"
+            }}
+          />
+        </div>
+        <div 
+          style={{
+            display: "flex",
+            width: "100%"
+          }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="question2"
+            label="Security Question 2"
+            name="question2"
+            disabled={isRegistering}
+            value={formik.values.question2}
+            onChange={formik.handleChange}
+            error={formik.touched.question2 && Boolean(formik.errors.question2)}
+            helperText={formik.touched.question2 && formik.errors.question2}
+            style={{
+              marginRight:"10px"
+            }}
+          />
 
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          id="answer1"
-          label="Security Answer 1"
-          name="answer1"
-          disabled={isRegistering}
-          value={formik.values.answer1}
-          onChange={formik.handleChange}
-          error={formik.touched.answer1 && Boolean(formik.errors.answer1)}
-          helperText={formik.touched.answer1 && formik.errors.answer1}
-        />
-        
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="answer2"
+            label="Security Answer 2"
+            name="answer2"
+            disabled={isRegistering}
+            value={formik.values.answer2}
+            onChange={formik.handleChange}
+            error={formik.touched.answer2 && Boolean(formik.errors.answer2)}
+            helperText={formik.touched.answer2 && formik.errors.answer2}
+            style={{
+              marginLeft:"10px"
+            }}
+          />
+        </div>
+        <div 
+          style={{
+            display: "flex",
+            width: "100%"
+          }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="question3"
+            label="Security Question 3"
+            name="question3"
+            disabled={isRegistering}
+            value={formik.values.question3}
+            onChange={formik.handleChange}
+            error={formik.touched.question3 && Boolean(formik.errors.question3)}
+            helperText={formik.touched.question3 && formik.errors.question3}
+            style={{
+              marginRight:"10px"
+            }}
+          />
+
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="answer3"
+            label="Security Answer 3"
+            name="answer3"
+            disabled={isRegistering}
+            value={formik.values.answer3}
+            onChange={formik.handleChange}
+            error={formik.touched.answer3 && Boolean(formik.errors.answer3)}
+            helperText={formik.touched.answer3 && formik.errors.answer3}
+            style={{
+              marginLeft:"10px"
+            }}
+          />
+        </div>
         <LoadingButton
           type="submit"
           fullWidth
@@ -206,6 +332,11 @@ const Register = () => {
           {t("auth.register.back")}
         </Button>
       </Box>
+      <Typography component="h3" variant="h5" style={{marginTop:"20px"}}>
+      **Not connected to backend yet
+        </Typography>
+      <div>
+      </div>
       </Grid>
   );
 };
