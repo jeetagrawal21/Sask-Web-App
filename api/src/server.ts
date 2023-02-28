807dc62b9b072a051f7029e75dd46098c224118f
ginsan11 <thezerodude2@gmail.com>
2023-02-27 21:29:13 -0600

    Finished linking up frontend functions to back to db

--- api/src/server.ts
+++ api/src/server.ts
@@ -53,7 +53,6 @@ const credentials= {
   port: 5432,
 };
 
-//createtable();
 
 // Connect with a connection pool.
 
@@ -77,10 +76,12 @@ async function clientDemo() {
   return now;
 }
 
-//function to create database and table 
+//createtable();  // RUN ONLY ONCE (if you have no tables on your computer otherwise dont run at all)
+
+//function to creates table under current database 
 async function createtable(){
   const pool = new Pool(credentials);
-  pool.query("CREATE TABLE users(id serial NOT NULL, username character varying(80) NOT NULL, pass character(40) NOT NULL, email character varying(100) NOT NULL, privilege integer NOT NULL DEFAULT 0, securityQuestion1 character(40) NOT NULL, securityAnswer1 character(40) NOT NULL, securityQuestion2 character(40) NOT NULL, securityAnswer2 character(40) NOT NULL, securityQuestion3 character(40) NOT NULL, securityAnswer3 character(40) NOT NULL, CONSTRAINT utilisateur_pkey PRIMARY KEY (id))", (err, res) => {
+  pool.query("CREATE TABLE users(id serial NOT NULL, surname character varying(80) NOT NULL, givenname2 character varying(80) NOT NULL, givenname3 character varying(80) NOT NULL, pass character varying(40) NOT NULL, email character varying(100) NOT NULL, privilege integer NOT NULL DEFAULT 0, securityQuestion1 character(40) NOT NULL, securityAnswer1 character(40) NOT NULL, securityQuestion2 character(40) NOT NULL, securityAnswer2 character(40) NOT NULL, securityQuestion3 character(40) NOT NULL, securityAnswer3 character(40) NOT NULL, CONSTRAINT utilisateur_pkey PRIMARY KEY (id))", (err, res) => {
     //console.log(err, res);
     pool.end();
 });
@@ -89,75 +90,62 @@ async function createtable(){
 //Checks if user exists (using email) return true if 1 user is found and felse otherwise
 async function checkifuser(email:string){
   const pool = new Pool(credentials);
-  const now = await pool.query(                    //query looks for all users with email
-    `SELECT * FROM users WHERE email = $1`, 
-        [email]);
+  const now = await pool.query(                   //query looks for all users with email and password
+    `SELECT EXISTS(SELECT 1 FROM users WHERE email=$1)`, [email]);
   pool.end();
-  if(now.rows.length == 1){          //if more then 1 user is returned from query, returns false for user not found (there should only be 1 user with the info)
-    return (true);
-  }else{
-    return false;
-  }
+  return Boolean(((now['rows'])[0])['exists']);  // typecast the return value to bool. Came back in array sorta thing thus all the nonsense
 }
 
-//Gets user and return user info. Returmns user info if 1 user is found, false otherwise
+//Gets user and return user info.
 async function getuser (email:string, pass:string){
   const pool = new Pool(credentials);
   const now = await pool.query(               //query looks for all users with email
-    `SELECT * FROM users WHERE email = $1`, 
+    `SELECT 1 FROM users WHERE email = $1`, 
         [email]);
   await pool.end();
-  if(now.rows.length == 1){         //if more then 1 user is returned from query, returns false for user not found (there should only be 1 user with the info)
-    return now;                    //user data returned 
-  }else{
-    return false;
-  }
+  return now;
 } 
 
 //Checks password for the associated email. Returns true for correct combo, false otherswise
 async function checkpass(email:string, pass:string){
   const pool = new Pool(credentials);
-  const now = await pool.query(                   //query looks for all users with email
-    `SELECT * FROM users WHERE email = $1`, 
-        [email]);
+  const now = await pool.query(                   //query looks for all users with email and password
+    `SELECT EXISTS(SELECT 1 FROM users WHERE email=$1 AND pass=$2)`, [email, pass]);
   pool.end();
-  if(now.rows.length == 1){     //if more then 1 user is returned from query, returns false for user not found (there should only be 1 user with the info)
-    console.log(now);
-    return (true);
-  }else{
-    return false;
-  }
+  return Boolean(((now['rows'])[0])['exists']);  // typecast the return value to bool. Came back in array sorta thing thus all the nonsense
+
 }
 
 //Creates account for admins (admins have a privilege of 1 as opposed to 0)
-async function accountcreationadmin(username:string, pass:string, email:string, securityQuestion1:string, securityAnswer1:string, securityQuestion2:string, securityAnswer2:string, securityQuestion3:string, securityAnswer3:string){
+async function accountcreationadmin(surname:string, givenname2:string, givenname3:string, pass:string, email:string, securityQuestion1:string, securityAnswer1:string, securityQuestion2:string, securityAnswer2:string, securityQuestion3:string, securityAnswer3:string){
   const pool = new Pool(credentials);
   if (await checkifuser(email)){                     //calls check if user exist (using email) if returns true he exist account not created else account created
     console.log('account not created');
   }else{
     const now = await pool.query(               //account created
-      `INSERT INTO users (username, pass, email, privilege, securityQuestion1, securityAnswer1, securityQuestion2, securityAnswer2, securityQuestion3, securityAnswer3  )  
-       VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`, [username, pass, email, 1, securityQuestion1, securityAnswer1, securityQuestion2, securityAnswer2, securityQuestion3, securityAnswer3]);
+      `INSERT INTO users (surname, givenname2, givenname3, pass, email, privilege, securityQuestion1, securityAnswer1, securityQuestion2, securityAnswer2, securityQuestion3, securityAnswer3)  
+       VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`, [surname, givenname2, givenname3, pass, email, 1, securityQuestion1, securityAnswer1, securityQuestion2, securityAnswer2, securityQuestion3, securityAnswer3]);
+       console.log("Account created");
   }
   await pool.end();
 }
 
 //Creates account for users (users dont need a privilige variable because it is 0 by default)
-async function accountcreationuser(ID:number, username:string, pass:string, email:string, securityQuestion1:string, securityAnswer1:string, securityQuestion2:string, securityAnswer2:string, securityQuestion3:string, securityAnswer3:string){
+async function accountcreationuser(ID:number, surname:string, givenname2:string, givenname3:string, pass:string, email:string, securityQuestion1:string, securityAnswer1:string, securityQuestion2:string, securityAnswer2:string, securityQuestion3:string, securityAnswer3:string){
   const pool = new Pool(credentials);
   if (await checkifuser(email)){              //calls check if user exist (using email) if returns true he exist account not created else account created
     console.log("account not created");
   }else{
     const now = await pool.query(            //account created         
-      `INSERT INTO users (id, username, pass, email, securityQuestion1, securityAnswer1, securityQuestion2, securityAnswer2, securityQuestion3, securityAnswer3  )  
-       VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9 $10)`, [ID, username, pass, email, securityQuestion1, securityAnswer1, securityQuestion2, securityAnswer2, securityQuestion3, securityAnswer3]);
-    console.log("Account not created");
+      `INSERT INTO users (id, surname, givenname2, givenname3, pass, email, securityQuestion1, securityAnswer1, securityQuestion2, securityAnswer2, securityQuestion3, securityAnswer3)  
+       VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`, [ID, surname, givenname2, givenname3, pass, email, securityQuestion1, securityAnswer1, securityQuestion2, securityAnswer2, securityQuestion3, securityAnswer3]);
+    console.log("Account created");
   }
   await pool.end();
 } 
 
 
-//changes password, returns true if succesful 
+//changes password, returns true if succesful    *****NEEEEDSS TESTING/REWRITE NOT SURE IF WORKS******
 async function changepass(email:string, pass:string){
   const pool = new Pool(credentials);
   if (await checkifuser(email)){             //calls check if user exist (using email) if returns true he exist account not created else account created
@@ -171,13 +159,9 @@ async function changepass(email:string, pass:string){
 }
 
 
-// //Sending query to the server
-// poolDemo();
+//used to create account  Use webpage instead if needed
+//accountcreationuser(12345678, 'testuser8', 'testusergiven1-8', 'testusergiven2-8', 'testpass8', 'testuser2@email.com', "hello who?", "World!", "Whats my name", "Testuser8", "Whats my purpose", "Testing");
 
-//accountcreationuser('testuser8', 'testpass8', 'testuser2@email.com', "hello who?", "World!", "Whats my name", "Testuser8", "Whats my purpose", "Testing");
-//account('testuser8', 'testpass8', 'testuser2@email.com', "hello who?", "World!", "Whats my name", "Testuser8", "Whats my purpose", "Testing");
-
-//console.log(checkifuser('testuser2@email.com'));
 
 
 // **** Add API routes **** //
@@ -214,7 +198,7 @@ app.get('/TestPage', function (req, res) {
  var partIDnum:number;
  app.post('/requestAccount', (req, res) => {
    const particpantId = req.body;
-   partIDnum = 12345; //Number(particpantId['participantId']);
+   partIDnum = Number(particpantId['participantId']);
    console.log(partIDnum);
  });
 
@@ -223,13 +207,15 @@ app.get('/TestPage', function (req, res) {
 app.post('/postregistrationinfo', (req, res) => {
   const data = req.body;
   //console.log(partIDnum, data['surname'] + data['givenName1'], data['password'], data['email'], data['question1'], data['answer1'], data['question2'], data['answer2'], data['question3'], data['answer3'])
-  //accountcreationuser(partIDnum, data['surname'] + data['givenName1'], data['password'], data['email'], data['question1'], data['answer1'], data['question2'], data['answer2'], data['question3'], data['answer3'])
+  accountcreationuser(partIDnum, data['surname'], data['givenName1'], data['givenName2'], data['password'], data['email'], data['question1'], data['answer1'], data['question2'], data['answer2'], data['question3'], data['answer3'])
   console.log(data);
 
   // call function to post data to the database
 
   res.send('success');
 });
+
+
 /**
  * Gets login data from the user
  * req: login data as a string array containing email and password
@@ -237,16 +223,27 @@ app.post('/postregistrationinfo', (req, res) => {
  */
 app.post('/login', (req, res) => {
   const data = JSON.stringify(req.body);
-  console.log(data);
+  async function checking () {  
+    var result:boolean = await checkpass(req.body.email, req.body.password);    //async function is required to make it wait for reply (await used below to specific what we wait for)
+    if (result){     //calls check password to see if pass and email match a database entry
+      console.log("login success!");
+    }else{
+      console.log("login failed");
+    }
+  }
+  checking(); //Calling above function with data from page
 
   // send data as JSON object to the function that aunthenticates login
   // send response if login was successful or not( we might not actually need the get request below)
 });
 
+
 app.get('/login', (req, res) => {
   // this function communicates with the login authentication function to check if login info is correct and return a response if it is found in the DB
   // res.send(req);
 });
+
+
 
 
 // **** Export default **** //
