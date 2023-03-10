0cdb43c6cc1667f0cce26e5bdf913af187b3642f
ginsan11 <thezerodude2@gmail.com>
2023-03-09 19:26:54 -0600

    added functions to check table and to initialize a table and add a default user
    Added checkiftable() and initiatedb() functions. initiatedb is rant automatically, creates table with a user if there are none present

--- api/src/server.ts
+++ api/src/server.ts
@@ -77,15 +77,25 @@ async function clientDemo() {
 }
 
 //createtable();  // RUN ONLY ONCE (if you have no tables on your computer otherwise dont run at all)
+async function checkiftable(){
+  const pool = new Pool(credentials);
+  const tablename = 'users'
+  const { rows } = await pool.query("SELECT EXISTS(SELECT FROM pg_catalog.pg_tables WHERE tablename  = $1)", [tablename]);
+  const exists = rows[0].exists;
+  pool.end();
+  return Boolean(exists);
+}
+
 
 //function to creates table under current database 
 async function createtable(){
   const pool = new Pool(credentials);
-  pool.query("CREATE TABLE users(id serial NOT NULL, surname character varying(80) NOT NULL, givenname2 character varying(80) NOT NULL, givenname3 character varying(80) NOT NULL, pass character varying(40) NOT NULL, email character varying(100) NOT NULL, privilege integer NOT NULL DEFAULT 0, securityQuestion1 character(40) NOT NULL, securityAnswer1 character(40) NOT NULL, securityQuestion2 character(40) NOT NULL, securityAnswer2 character(40) NOT NULL, securityQuestion3 character(40) NOT NULL, securityAnswer3 character(40) NOT NULL, CONSTRAINT utilisateur_pkey PRIMARY KEY (id))", (err, res) => {
+  await pool.query("CREATE TABLE users(id serial NOT NULL, surname character varying(80) NOT NULL, givenname2 character varying(80) NOT NULL, givenname3 character varying(80) NOT NULL, pass character varying(40) NOT NULL, email character varying(100) NOT NULL, privilege integer NOT NULL DEFAULT 0, securityQuestion1 character(40) NOT NULL, securityAnswer1 character(40) NOT NULL, securityQuestion2 character(40) NOT NULL, securityAnswer2 character(40) NOT NULL, securityQuestion3 character(40) NOT NULL, securityAnswer3 character(40) NOT NULL, CONSTRAINT utilisateur_pkey PRIMARY KEY (id))", (err, res) => {
     //console.log(err, res);
     pool.end();
 });
 }
+
 
 //Checks if user exists (using email) return true if 1 user is found and felse otherwise
 async function checkifuser(email:string){
@@ -160,6 +170,42 @@ async function changepass(email:string, newpass:string){
 
 //used to create account  Use webpage instead if needed
 //accountcreationuser(12345678, 'testuser8', 'testusergiven1-8', 'testusergiven2-8', 'testpass8', 'testuser2@email.com', "hello who?", "World!", "Whats my name", "Testuser8", "Whats my purpose", "Testing");
+/**
+ * This is an async function that checks if the "users" table exists in the database.
+ * If the table does not exist, it creates the table and inserts a new user.
+ * @returns {Promise<void>} Nothing is returned from this function.
+ */
+// This is an async function named "test".
+async function initiatedb() {
+  const pool = new Pool(credentials);
+  const tablename = 'users';
+  // Call "checkiftable" function and wait for it to complete, storing the result in "tablecheck" variable.
+  const tablecheck = await checkiftable();
+  if (!tablecheck){
+    try {
+      // Execute a SQL query to create a table named "users" if it doesn't already exist.
+      await pool.query('CREATE TABLE IF NOT EXISTS users (id serial NOT NULL, surname character varying(80) NOT NULL, givenname2 character varying(80) NOT NULL, givenname3 character varying(80) NOT NULL, pass character varying(40) NOT NULL, email character varying(100) NOT NULL, privilege integer NOT NULL DEFAULT 0, securityQuestion1 character(40) NOT NULL, securityAnswer1 character(40) NOT NULL, securityQuestion2 character(40) NOT NULL, securityAnswer2 character(40) NOT NULL, securityQuestion3 character(40) NOT NULL, securityAnswer3 character(40) NOT NULL, CONSTRAINT utilisateur_pkey PRIMARY KEY (id));');
+      // Execute a SQL query to check if the "users" table exists, and store the result in "res".
+      const res = await pool.query(`SELECT EXISTS(SELECT * FROM information_schema.tables WHERE table_name='${tablename}')`);
+      // Log the value of the "exists" column in the first row of the "res" result.
+      console.log(res.rows[0].exists);
+      console.log("Table was created successfully.");
+      // Call the "accountcreationuser" function with some parameters.
+      await accountcreationuser(12345678, 'testuser1', 'testusergiven1-1', 'testusergiven2-1', 'testpass1', 'testuser1@email.com', "hello who?", "World!", "Whats my name", "Testuser1", "Whats my purpose", "Testing");
+
+
+    } catch (error) {
+      // If an error occurs, log it to the console.
+      console.error(error);
+    } finally {
+      pool.end();
+    }
+
+  }
+
+}
+
+initiatedb();
 
 
 
