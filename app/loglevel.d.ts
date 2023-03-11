3308f5e75933588eb88ba3eb5d81c23a423d7a18
Riley Chometa <RPC619@USASK.CA>
2023-03-11 11:03:47 -0600

    installed loglevel and the loglevel remote plugin so i can send logs to the backend. configured it so that you do not even have to import it.

---
+++ app/loglevel.d.ts
@@ -0,0 +1,3 @@
+// In loglevel.d.ts
+declare var log: import("loglevel").Logger;
+declare var remote: import("loglevel-plugin-remote").remote;
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -10,6 +10,21 @@ import Timeline from './pages/Timeline';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import Dashboard from './pages/Dashboard';
 
+
+const remoteOptions = {
+  url: "https://my-server.com/crash-reports",
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  level: process.env.NODE_ENV === "development"?
+                                  "trace": // Send trace level messages and above when in development
+                                  "error", // Only send error level messages when in production
+};
+
+remote.apply(log, remoteOptions);
+
+
 function App() {
   return (
     <Router>
