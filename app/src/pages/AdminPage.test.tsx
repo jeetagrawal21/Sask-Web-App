b30be314ceeb9424544dbf36b6ded47c411ec477
chii-vu <chi.vu@usask.ca>
2023-03-11 16:11:27 -0600

    Added unit tests for AdminPage component
    The added unit tests cover the functionality of rendering user requests and all users tables, approving and rejecting user requests, and removing users from the all users table. The tests mock initial data and make use of the , , and  functions from . The  and  functions are used to mock the  function and restore it to its original state between tests.

--- app/src/pages/AdminPage.test.tsx
+++ app/src/pages/AdminPage.test.tsx
@@ -1,60 +1,41 @@
 import React from "react";
 import { render, fireEvent, waitFor } from "@testing-library/react";
-import AdminPage, { User, UserRequest } from "./AdminPage";
+import AdminPage, { UserRequest, User } from "./AdminPage";
 
-// Mock data for testing
-const mockUserRequests: UserRequest[] = [
+// mock initial data
+const initUserRequests: UserRequest[] = [
   {
-    id: 1,
-    name: "John Doe",
-    email: "john.doe@example.com",
+    id: 2735,
+    name: "Jack Black",
+    email: "jack.black@example.com",
     requestDate: new Date(),
   },
   {
-    id: 2,
-    name: "Jane Smith",
-    email: "jane.smith@example.com",
+    id: 1892,
+    name: "Mary White",
+    email: "mary.white@example.com",
     requestDate: new Date(),
   },
 ];
 
-const mockUsers: User[] = [
-  {
-    id: 1,
-    name: "John Doe",
-    email: "john.doe@example.com",
-  },
-  {
-    id: 2,
-    name: "Jane Smith",
-    email: "jane.smith@example.com",
-  },
+const initUsers: User[] = [
+  { id: 3921, name: "John Doe", email: "john.doe@example.com" },
+  { id: 2012, name: "Jane Smith", email: "jane.smith@example.com" },
+  { id: 3289, name: "Bob Johnson", email: "bob.johnson@example.com" },
 ];
 
-// Mock fetch functions for testing
-const mockFetchUserRequests = jest.fn(() => Promise.resolve(mockUserRequests));
-const mockFetchAllUsers = jest.fn(() => Promise.resolve(mockUsers));
-const mockHandleApproveUserRequest = jest.fn();
-const mockHandleRejectUserRequest = jest.fn();
-const mockHandleRemoveUser = jest.fn();
-
-// Mock the useState hook
-jest.mock("react", () => ({
-  ...jest.requireActual("react"),
-  useState: jest.fn(),
-}));
-
-
-// Mock the useEffect hook
 describe("AdminPage", () => {
   beforeEach(() => {
-    // Reset the mock functions and the state
-    jest.clearAllMocks();
-    (React.useState as jest.Mock).mockReturnValueOnce([
-      [],
-      mockFetchUserRequests,
-    ]);
-    (React.useState as jest.Mock).mockReturnValueOnce([[], mockFetchAllUsers]);
+    // wait for initial data to load before each test
+    jest.spyOn(global, "fetch").mockImplementation(() =>
+      Promise.resolve({
+        json: () =>
+          Promise.resolve({
+            userRequests: initUserRequests,
+            allUsers: initUsers,
+          }),
+      } as Response)
+    );
   });
 
   // Test that the page renders the correct headings
@@ -67,13 +48,80 @@ describe("AdminPage", () => {
     });
   });
 
-  // Test that the fetch functions are called on mount
-  it("calls fetch functions on mount", async () => {
-    render(<AdminPage />);
+  // Test that a user is added to the all users table when approved
+  it("should approve a user request and add user to all users table", async () => {
+    const { getByText, getByTestId, queryByText } = render(<AdminPage />);
+    // wait for initial data to load
     await waitFor(() => {
-      expect(mockFetchUserRequests).toHaveBeenCalledTimes(1);
-      expect(mockFetchAllUsers).toHaveBeenCalledTimes(1);
+      expect(getByText("User Requests")).toBeInTheDocument();
+      expect(getByText("All Users")).toBeInTheDocument();
+    });
+
+    // find the Approve button for the first user request and click it
+    const approveButton = getByTestId(`approve-user-${initUserRequests[0].id}`);
+    fireEvent.click(approveButton);
+
+    // check that the user has been removed from the user requests table
+    await waitFor(() => {
+      expect(
+        queryByText(`user-${initUserRequests[0].id}`)
+      ).not.toBeInTheDocument();
     });
   });
+
+  // Test that a user is removed from the user requests table when rejected
+  it("should reject a user request and remove it from user requests table", async () => {
+    const { getByText, getByTestId, queryByText } = render(<AdminPage />);
+    // wait for initial data to load
+    await waitFor(() => {
+      expect(getByText("User Requests")).toBeInTheDocument();
+      expect(getByText("All Users")).toBeInTheDocument();
+    });
+
+    // find the Reject button for the first user request and click it
+    const rejectButton = getByTestId(`reject-user-${initUserRequests[0].id}`);
+    fireEvent.click(rejectButton);
+
+    // check that the user has been removed from the user requests table
+    await waitFor(() => {
+      expect(
+        queryByText(`user-${initUserRequests[0].id}`)
+      ).not.toBeInTheDocument();
+    });
+  });
+
+  // Test that a user is removed from the all users table
+  it("should remove a user from the all users table", async () => {
+    const { getByTestId, queryByText } = render(<AdminPage />);
+
+    // find the Remove button for the first user and click it
+    const removeButton = getByTestId(`remove-user-${initUsers[0].id}`);
+    fireEvent.click(removeButton);
+
+    // check that the user has been removed from the all users table
+    await waitFor(() => {
+      expect(queryByText(`user-${initUsers[0].id}`)).not.toBeInTheDocument();
+    });
+  });
+
+  // Test that all users can be removed from the users table
+  it("should remove multiple users from the all users table", async () => {
+    const { getByText, getByTestId, queryByText } = render(<AdminPage />);
+    // wait for initial data to load
+    await waitFor(() => {
+      expect(getByText("User Requests")).toBeInTheDocument();
+      expect(getByText("All Users")).toBeInTheDocument();
+    });
+
+    // find all Remove buttons for and click them, checking that the user has been removed from the all users table
+    initUsers.forEach((user) => {
+      const removeButton = getByTestId(`remove-user-${user.id}`);
+      fireEvent.click(removeButton);
+      expect(queryByText(`user-${user.id}`)).not.toBeInTheDocument();
+    });
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
 });
-  
