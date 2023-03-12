3f10ef01e5a91adf6cbe5a92b44f0ef41b3aa13d
ginsan11 <94964526+ginsan11@users.noreply.github.com>
2023-03-11 18:13:39 -0600

    Installed nivo and updated graph.tsx
    Graph component shows generic graph for now
    
    Ran:
    npm install nico
    npm add @nivo/core @nivo/line

--- app/src/components/DashboardComponents/Graph.tsx
+++ app/src/components/DashboardComponents/Graph.tsx
@@ -1,7 +1,46 @@
+import { Line } from '@nivo/line';
 import React from 'react';
 
-function Graph() {
-  return <div></div>;
-}
+const commonProperties = {
+    width: 800,
+    height: 400,
+    margin: { top: 50, right: 110, bottom: 50, left: 60 },
+    enableGridX: false,
+    enableGridY: false,
+};
 
-export default Graph;
+const MyLineChart = () => (
+    <Line
+        {...commonProperties}
+        curve="monotoneX"
+        data={[
+            {
+                id: 'fake corp. A',
+                data: [
+                    { x: 0, y: 7 },
+                    { x: 1, y: 5 },
+                    { x: 2, y: 11 },
+                    { x: 3, y: 9 },
+                    { x: 4, y: 13 },
+                    { x: 7, y: 16 },
+                    { x: 9, y: 12 },
+                ],
+            },
+        ]}
+        xScale={{
+            type: 'linear',
+            min: 0,
+            max: 'auto',
+        }}
+        axisLeft={{
+            legend: 'linear scale',
+            legendOffset: 12,
+        }}
+        axisBottom={{
+            legend: 'linear scale',
+            legendOffset: -12,
+        }}
+    />
+);
+
+export default MyLineChart;
