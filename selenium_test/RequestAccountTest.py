db4e5c12b19a921ebaead03cbf9f8bb61f89c39f
Inioluwa Olaleye <olaleyeini7@gmail.com>
2023-03-12 05:28:55 -0600

    wrote intgration tests for the request account page

---
+++ selenium_test/RequestAccountTest.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+import sys
+
+print("Test Execution Started")
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-ssl-errors=yes')
+options.add_argument('--ignore-certificate-errors')
+driver = webdriver.Remote(
+command_executor='http://localhost:4444/wd/hub',
+options=options
+)
+#maximize the window size
+driver.maximize_window()
+time.sleep(5)
+#navigate to browserstack.com
+driver.get("http://covid_app:3080")
+time.sleep(3)
+requestAccount = driver.find_element(By.CLASS_NAME, "request-account-link")
+requestAccount.click()
+time.sleep(2)
+# check to see if it gets to the right page
+expected_url = "http://covid_app:3080/RequestAccount"
+if (driver.current_url != expected_url):
+    print('Error expected the page to be at ' + expected_url + " but it's at " + driver.current_url)
+    time.sleep(2)
+    driver.close()
+    driver.quit()
+    print("Test failed")
+    sys.exit() 
+
+driver.close()
+driver.quit()
+print("Test Execution Successfully Completed!")
