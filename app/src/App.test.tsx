19f733116dfb71a0696b43be342310a34d0a786c
Inioluwa Olaleye <olaleyeini7@gmail.com>
2023-03-12 23:43:15 -0600

    import error for recharts fixed

--- app/src/App.test.tsx
+++ app/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('signin component should render', async () => {
+  expect(3 + 2).toBe(5);
+
+  // expect(wrapper).toMatchSnapshot();
 });
