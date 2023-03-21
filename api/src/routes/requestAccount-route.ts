f798d6450fad68c66368cfbfc35dfaade6d316e0
ginsan11 <94964526+ginsan11@users.noreply.github.com>
2023-03-21 14:51:00 -0500

    variable value change

--- api/src/routes/requestAccount-route.ts
+++ api/src/routes/requestAccount-route.ts
@@ -1,30 +1,32 @@
+import express from 'express';
 import { Request, Response } from 'express';
 import session from 'express-session';
-import express from 'express';
 
-interface SessionData {
-  partIDnum: number;
-}
 
-declare module 'express-session' {
-  interface SessionData {
-    partIDnum: number;
-  }
-}
 
 const router = express.Router();
 
-router.use(session({
-  secret: 'your-secret-key',
-  resave: false,
-  saveUninitialized: true
-}));
 
+
+// Add express-session middleware
+router.use(session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: true
+  }));
+
+
+
+/**
+ * for now this would not really do any work on the backend other than store participant id with the rest of particiapnt info on the registration page
+ * in the future we plan for it to send this participnat id to the admin who then verifies and send a unique link to the participant for registration
+ */
 router.get('/requestAccount', (req: Request, res: Response) => {
   const particpantId = req.body;
-  req.session.partIDnum = Number(particpantId['participantId']);
+  const partIDnum = 753951
+  //req.session.partIDnum = Number(particpantId['participantId']);
   console.log('PRINTED HERE');
-  console.log(req.session.partIDnum);
+  console.log(partIDnum);
 });
 
 export default router;
