305cf204879fe04db941c9e751455ea22eb81262
ginsan11 <thezerodude2@gmail.com>
2023-03-22 17:48:09 -0600

    Updated test cases to match updates

--- api/__tests__/dataServicesTests.ts
+++ api/__tests__/dataServicesTests.ts
@@ -1,28 +1,23 @@
-import {getdata, checkifadmin, credentials} from "../src/server"
+import {getData, checkifadmin, credentials} from "../src/services/Data-Services"
 import { Client, Pool } from 'pg';
 
 
-//import checkifadmin from "../src/server"
-// import getuser from "../src/server"
-// import checkpass from "../src/server"
-// import accountcreationadmin from "../src/server"
-// import accountcreationuser from "../src/server"
 
 
-describe("getdata function", () => {
-  test("returns formatted data for valid id", async () => {
+describe("getData function", () => {
+  it("returns formatted data for valid id", async () => {
     const id = 42176;
-    const result = await getdata(id);
+    const result = await getData(id);
     // Define expected data
     const expectedData = [      { name: 'Feb', 'Cough severity': 4, uv: 4 },      { name: 'Mar', 'Cough severity': 2, uv: 2 },      { name: 'Apr', 'Cough severity': 1, uv: 1 },      { name: 'May', 'Cough severity': 0, uv: 0 }    ];
     expect(result).toEqual(expect.arrayContaining(expectedData));
-    console.log("getdata function: valid id - PASSED");
+    console.log("getData function: valid id - PASSED");
   });
   
-  test("returns empty array for invalid id", async () => {
+  it("returns empty array for invalid id", async () => {
     const id = 9999; // An invalid id
-    const result = await getdata(id);
+    const result = await getData(id);
     expect(result).toEqual([]);
-    console.log("getdata function: invalid id - PASSED");
+    console.log("getData function: invalid id - PASSED");
   });
 });
