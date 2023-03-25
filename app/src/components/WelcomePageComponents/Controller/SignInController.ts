7ec9b5d4f57c887946bb2069dfc12bcad27a299e
chii-vu <chi.vu@usask.ca>
2023-03-25 13:08:30 -0600

    Updated password regex and function headers

--- app/src/components/WelcomePageComponents/Controller/SignInController.ts
+++ app/src/components/WelcomePageComponents/Controller/SignInController.ts
@@ -1,38 +1,38 @@
-import React, { Component, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import axios from 'axios';
-
 /**
-   Purpose: Checks if the format of a given email is valid.
-   Precondition: email: email string entered by the user
-   PostCondition: None
-   Return: true if the email provided by the user matches the description provided in the function  
+ * Check if the format of a given email is valid.
+ * @param email email string entered by the user
+ * @returns true if the email provided by the user matches the description provided in the function
 */
 function checkEmail(email: string) {
   //Email should not have any spaces before or after you type and it should have a single "@" character and it can end in any domain (protonmail.com, protonmail.de, gmail.com, gmail.in) but it cannot have more than 3 letters after the '.'
-  const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  const emailRegex = /^\s*[\w\-\+_]+(\.[\w\-\+_]+)*\@[\w\-\+_]+\.[\w\-\+_]{2,3}\s*$/;
   return emailRegex.test(email);
 }
 
 /**
-   Purpose: Checks if the format of a given password is valid.
-   Precondition: password: password string entered by the user
-   PostCondition: None
-   Return: true if the password provided by the user matches the description provided in the function  
-*/
-function checkPassword(password: string) {
-  //Password should be a minimum of 8 characters with at least one number and one special character
-  const passwordRegex =
-    /^(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-  return passwordRegex.test(password);
+ * Checks if the format of a given password is valid. 
+ * @param password password string entered by the user
+ * @returns true if the password provided by the user matches the description provided in the function
+ */
+function checkPassword(password: string): boolean {
+  let count = 0;
+
+  if (password.length >= 8 && password.length <= 32) {
+    if (/\d/.test(password)) count++;
+    if (/[a-z]/.test(password)) count++;
+    if (/[A-Z]/.test(password)) count++;
+    if (/[*.!@#$%^&(){}\[\]:;"'<>,.?\/~`_+\-=|\\]/.test(password)) count++;
+  }
+
+  return count >= 3;
 }
 
 /**
-   Purpose: Disables a button if the requirements specified are not fulfilled
-   Precondition: email: password string entered by the user, password: password string entered by the user
-   PostCondition: None
-   Return: true if any of the inputs do not meet the requirement   
-*/
+  * Checks if the email and password are valid and returns the opposite of that.
+  * @param email email string entered by the user
+  * @param password password string entered by the user
+  * @returns true if the email and password are valid, false otherwise
+  */
 function handleDisable(email: string, password: string): boolean {
   let result = checkEmail(email) && checkPassword(password);
   return !result;
