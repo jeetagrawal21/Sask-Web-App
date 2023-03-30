9f841a1d5e082ed3f3badf7525b062493b6f5987
Inioluwa Olaleye <olaleyeini7@gmail.com>
2023-03-30 16:54:40 -0600

    wrap the app and the route in the authprovider tag

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,19 +1,20 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./App.css";
-import WelcomePage from "./pages/WelcomePage";
-import Register from "./pages/RegisterPage";
-import RequestAccount from "./pages/RequestAccountPage";
-import AdminPage from "./pages/AdminPage";
-import Dashboard from "./pages/DashboardPage";
-import log from "loglevel";
-import remotePlugin from "loglevel-plugin-remote";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import WelcomePage from './pages/WelcomePage';
+import Register from './pages/RegisterPage';
+import RequestAccount from './pages/RequestAccountPage';
+import AdminPage from './pages/AdminPage';
+import Dashboard from './pages/DashboardPage';
+import { AuthProvider } from './AuthContext';
+import log from 'loglevel';
+import remotePlugin from 'loglevel-plugin-remote';
 
 const remoteOptions = {
-  url: process.env.REACT_APP_API_BASE_URL + "/logs",
+  url: process.env.REACT_APP_API_BASE_URL + '/logs',
   format: remotePlugin.json,
-  method: "POST",
-  level: "trace", // Only send trace level messages and above
+  method: 'POST',
+  level: 'trace', // Only send trace level messages and above
 };
 
 remotePlugin.apply(log, remoteOptions); // use remotePlugin.apply() here
@@ -23,13 +24,15 @@ log.enableAll();
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<WelcomePage />} />
-        <Route path="/RequestAccount" element={<RequestAccount />} />
-        <Route path="/Register" element={<Register />} />
-        <Route path="/Dashboard" element={<Dashboard />} />
-        <Route path="/AdminPage" element={<AdminPage />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="/RequestAccount" element={<RequestAccount />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/AdminPage" element={<AdminPage />} />
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 }
