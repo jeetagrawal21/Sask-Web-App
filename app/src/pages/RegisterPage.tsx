763382325f614f33a8eb3afc9434ad0681e7535e
Inioluwa Olaleye <olaleyeini7@gmail.com>
2023-03-31 11:51:16 -0600

    prevent navigation to register page if not approved

--- app/src/pages/RegisterPage.tsx
+++ app/src/pages/RegisterPage.tsx
@@ -1,16 +1,39 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
 import '../App.css';
 import RegisterPageTitle from '../components/RegisterPageComponents/RegisterPageTitle';
 import RegisterPageBody from '../components/RegisterPageComponents/RegisterPageBody';
+import { AuthContext } from '../AuthContext';
+import { Link, useNavigate } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <RegisterPageTitle />
-      <RegisterPageBody />
-    </div>
-  );
+  const navigate = useNavigate();
+
+  /**
+   * Purpose: Return the user to the welcome page
+   * Preconditions: None
+   * Postconditions: None
+   * Return: None
+   */
+  function returnToWelcomePage() {
+    navigate('/');
+  }
+
+  const { isAllowedToRegister } = useContext(AuthContext); // Get the authentication status from the context
+  if (!isAllowedToRegister) {
+    return (
+      <div>
+        <h1>Unauthorized access</h1>
+        <button onClick={returnToWelcomePage}>Return to home page</button>
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <RegisterPageTitle />
+        <RegisterPageBody />
+      </div>
+    );
+  }
 }
 
 export default App;
