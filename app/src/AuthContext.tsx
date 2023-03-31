b177126d4e01d2c5c3f9202e28daca534caa7cd1
Inioluwa Olaleye <olaleyeini7@gmail.com>
2023-03-31 11:50:28 -0600

    add authentication state for registration

--- app/src/AuthContext.tsx
+++ app/src/AuthContext.tsx
@@ -11,16 +11,27 @@ export const AuthContext = createContext({
   isAuthenticated: false,
   // setAuthenticated function will default to a no-op function
   setAuthenticated: (value: boolean) => {},
+
+  isAllowedToRegister: false,
+  setAllowedToRegister: (value: boolean) => {},
 });
 
 // Define the AuthProvider component that wraps the app and provides the AuthContext object
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   // Use the useState hook to set the initial state of isAuthenticated to false
   const [isAuthenticated, setAuthenticated] = useState(false);
+  const [isAllowedToRegister, setAllowedToRegister] = useState(false);
 
   // Return the AuthContext.Provider component with the value of isAuthenticated and setAuthenticated functions
   return (
-    <AuthContext.Provider value={{ isAuthenticated, setAuthenticated }}>
+    <AuthContext.Provider
+      value={{
+        isAuthenticated,
+        setAuthenticated,
+        isAllowedToRegister,
+        setAllowedToRegister,
+      }}
+    >
       {/* Render the children components passed to AuthProvider */}
       {children}
     </AuthContext.Provider>
