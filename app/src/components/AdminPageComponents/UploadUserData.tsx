92b95ce4d7bdc168bf458cbdb06e5599a9ba8484
Your Name <nnsashti2000@protonmail.com>
2023-04-06 00:17:20 -0600

    Upload button finally works as intended. Hurray!!

--- app/src/components/AdminPageComponents/UploadUserData.tsx
+++ app/src/components/AdminPageComponents/UploadUserData.tsx
@@ -12,19 +12,61 @@ function UploadUserData() {
 
   function handleUploadUserData() {
     // TODO: implement Upload user data logic
-    localStorage.clear();
+    // const file = event.target.files[0]; // get the uploaded file
+    // console.log("Uploaded file:", file);
 
     // redirect to home page
     // navigate("/");
   }
 
   return (
-    // <button type="button" onClick={handleUploadUserData}>
-    <button type="button">
-    {/* <button type="file" name="file" id="file" style={{ display: "none" }} /> */}
-      Upload user data
-    </button>
+    <div>
+      <label htmlFor="upload-file" style={{ display: "none" }}>
+        Upload user data
+      </label>
+      <input type="file" id="upload-file" name="upload-file" style={{ display: "none" }} onChange={handleUploadUserData}/>
+      <button
+        type="button"
+        onClick={() => {
+          document.getElementById("upload-file")?.click(); // trigger file explorer
+        }}
+      >
+        Upload user data
+      </button>
+    </div>
   );
 }
 
 export default UploadUserData;
+
+
+
+// import React from "react";
+// import { useNavigate } from "react-router-dom";
+
+// /**
+//  * Sign out button to clear local storage and redirect to home page
+//  * @precond user is logged in
+//  * @postcond user is logged out
+//  * @returns sign out button
+//  */
+// function UploadUserData() {
+//   const navigate = useNavigate();
+
+//   function handleUploadUserData() {
+//     // TODO: implement Upload user data logic
+//     localStorage.clear();
+
+//     // redirect to home page
+//     // navigate("/");
+//   }
+
+//   return (
+//     // <button type="button" onClick={handleUploadUserData}>
+//     <button type="button">
+//       Upload user data
+//     </button>
+//   );
+// }
+
+// export default UploadUserData;
